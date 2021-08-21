4cb11b09be1b3dbb170a8b3902d9e5e26e0cd723
KuberTheGreat <73421387+KuberTheGreat@users.noreply.github.com>
2021-08-21 18:26:50 +0530

    Add files via upload

---
+++ Standard Deviation/deviation.py
@@ -0,0 +1,37 @@
+import csv
+import math
+
+with open('Standard Deviation/data.csv', newline = '') as f:
+    reader = csv.reader(f)
+    fileData = list(reader)
+    
+data = fileData[0]
+
+def mean(data):
+    n = len(data)
+
+    total = 0
+
+    for x in data:
+        total += int(x)
+
+    mean = total / n
+
+    return mean
+
+squaredList = []
+
+for number in data:
+    a = int(number) - mean(data)
+    a = a ** 2
+    squaredList.append(a)
+
+sum = 0
+
+for i in squaredList:
+    sum = sum + i
+
+result = sum / (len(data) - 1)
+
+standardDeviation = math.sqrt(result)
+print('This is the standard deviation of the given data: ', standardDeviation)
